c24c0a02b2d4a2c87aaed437cf2c1915c3c61edc
swizz <emailswizz@gmail.com>
2025-05-28 02:59:44 -0400

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import Navbar from '../components/Navbar';
 import { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -45,6 +46,7 @@ export default function App({ Component, pageProps }) {
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
